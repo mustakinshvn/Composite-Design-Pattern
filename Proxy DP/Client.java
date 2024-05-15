4c56702a6415d891ac2e621d90a5ee46b1af33fd
Md Mustakin Ebne Mazid <108215796+mustakinshvn@users.noreply.github.com>
2024-05-15 21:06:37 +0600

    Add files via upload

---
+++ Proxy DP/Client.java
@@ -0,0 +1,10 @@
+public class Client {
+    public static void main(String[] args) {
+        
+        ProxyShell proxyShell = new ProxyShell();
+        proxyShell.run("mkdir design pattern", "user");
+        proxyShell.run("rm -rf", "user");
+        proxyShell.run("rm -rf", "admin");
+        
+    }
+}
